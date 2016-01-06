70366c8e766081db21c49b9f161c29b19dd870e4
evanhowerton <evanhowerton@gmail.com>
2016-01-06 16:16:09 -0500

    First file

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BlueShortModifiedVoltage.java
@@ -0,0 +1,183 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+
+public class BlueShortModifiedVoltage extends LinearOpMode {
+    DcMotor motorRightA;
+    DcMotor motorRightB;
+    DcMotor motorLeftA;
+    DcMotor motorLeftB;
+    DcMotor tapeExt;
+    Servo clawBody;
+    Servo trigger;
+    Servo tapeAngle;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        motorRightA = hardwareMap.dcMotor.get("motor_fr");
+        motorRightB = hardwareMap.dcMotor.get("motor_br");
+        motorLeftA = hardwareMap.dcMotor.get("motor_fl");
+        motorLeftB = hardwareMap.dcMotor.get("motor_bl");
+        tapeExt = hardwareMap.dcMotor.get("motor_ext");
+        motorLeftA.setDirection(DcMotor.Direction.REVERSE);
+        motorLeftB.setDirection(DcMotor.Direction.REVERSE);
+
+        clawBody = hardwareMap.servo.get("servo_1");
+        trigger = hardwareMap.servo.get("servo_2");
+        tapeAngle = hardwareMap.servo.get("servo_3");
+
+
+        trigger.setPosition(.5);
+        clawBody.setPosition(1);
+
+
+        // Wait for the start button to be pressed
+        waitForStart();
+        drive(0, 1, .5);
+        drive(.5, 1, .5);
+        turn(-46, 1, .5);
+        drive(6.5, 1, .5);
+        turn(-52, 1, .5);
+        blueRamp();
+
+    }
+
+    public void drive(double dist, double pow, double pause) throws InterruptedException {
+
+        double maxVel= 2.31;
+        double voltage = this.hardwareMap.voltageSensor.iterator().next().getVoltage();
+        double voltAdj = 0;
+
+        if (voltage<=.1){
+            voltAdj= 12/(66.8*voltage);
+        }
+        if (voltage>.1){
+            voltAdj= 12/(12.051656/(1+2.464978*(Math.pow(2.71828, (voltage*(-11.2111*voltage))))));
+        }
+
+        motorLeftA.setPower(-pow);
+        motorRightA.setPower(-pow);
+        motorLeftB.setPower(-pow);
+        motorRightB.setPower(-pow);
+
+        sleep((long) (1000 * (Math.abs(dist / (maxVel * pow))) * voltAdj ));
+
+        motorLeftA.setPower(0);
+        motorRightA.setPower(0);
+        motorLeftB.setPower(0);
+        motorRightB.setPower(0);
+
+        sleep((long) (pause * 1000));
+    }
+
+    public void turn(double theta, double pow, double pause) throws InterruptedException {
+
+        double period = 3.2;
+
+        if(theta>0){
+            motorLeftA.setPower(-pow);
+            motorRightA.setPower(pow);
+            motorLeftB.setPower(-pow);
+            motorRightB.setPower(pow);
+        }
+
+        if(theta<0){
+            motorLeftA.setPower(pow);
+            motorRightA.setPower(-pow);
+            motorLeftB.setPower(pow);
+            motorRightB.setPower(-pow);
+        }
+
+        sleep((long) (1000 * Math.abs((theta / 360) * period)));
+
+        motorLeftA.setPower(0);
+        motorRightA.setPower(0);
+        motorLeftB.setPower(0);
+        motorRightB.setPower(0);
+
+        sleep((long)(pause*1000));
+
+    }
+    public void claw(double pause) throws InterruptedException {
+
+        clawBody.setPosition(0);
+
+        sleep(1000);
+
+        clawBody.setPosition(1);
+
+        sleep((long) (pause * 1000));
+
+    }
+
+    public void setAngle(double theta, double pause) throws InterruptedException{
+        double angle = (3*theta) / 180;
+        angle = Range.clip(angle, 0, 1);
+        tapeAngle.setPosition(angle);
+
+        sleep((long) (pause * 1000));
+
+    }
+
+    public void tapeExt(double length, double pow, double pause) throws InterruptedException {
+
+        double maxVel=2;
+
+        tapeExt.setPower(-pow);
+        sleep((long) (1000 * (Math.abs(length / (maxVel * pow)))));
+
+        tapeExt.setPower(0);
+        sleep((long) (pause * 1000));
+
+    }
+
+    public void tapePull(double length, double pow, double pause) throws InterruptedException{
+        double maxVel=2;
+
+        tapeExt.setPower(-pow);
+        motorLeftA.setPower(pow);
+        motorRightA.setPower(pow);
+        motorLeftB.setPower(pow);
+        motorRightB.setPower(pow);
+        sleep((long) (1000 * (Math.abs(length / (maxVel * pow)))));
+
+        tapeExt.setPower(0);
+        motorLeftA.setPower(0);
+        motorRightA.setPower(0);
+        motorLeftB.setPower(0);
+        motorRightB.setPower(0);
+        sleep((long) (pause * 1000));
+    }
+
+    public void end() throws InterruptedException {
+
+        motorLeftA.setPowerFloat();
+        motorLeftB.setPowerFloat();
+        motorRightA.setPowerFloat();
+        motorRightB.setPowerFloat();
+
+    }
+
+    public void blueRamp() throws InterruptedException {
+        drive(1, -1, .5);
+        turn(50, 1, .5);
+        drive(2.4, -1, .5);
+        turn(-90, 1, .5);
+        drive(3.5, 1, 1);
+
+        setAngle(60.0, .5);
+        tapeExt(2.5,1,1.5);
+        setAngle(0.0,4);
+        tapePull(5,-.5,.5);
+    }
+
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDrive.java
@@ -0,0 +1,220 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * TeleOp Mode
+ * <p>
+ * Enables control of the robot via the gamepad
+ */
+public class FourWheelDrive extends OpMode {
+
+    /*
+     * Note: the configuration of the servos is such that
+     * as the arm servo approaches 0, the arm position moves up (away from the floor).
+     * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
+     */
+    // TETRIX VALUES.
+
+
+
+    DcMotor motorRightA;
+    DcMotor motorRightB;
+    DcMotor motorLeftA;
+    DcMotor motorLeftB;
+    DcMotor tapeExt;
+    Servo clawBody;
+    Servo trigger;
+    Servo tapeAngle;
+    Servo plow;
+
+    final static double tapeDelta = .0075;
+    double tapePosition = 0;
+
+
+    /**
+     * Constructor
+     */
+    public FourWheelDrive() {
+
+    }
+
+    /*
+     * Code to run when the op mode is initialized goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#init()
+     */
+    @Override
+    public void init() {
+
+
+		/*
+		 * Use the hardwareMap to get the dc motors and servos by name. Note
+		 * that the names of the devices must match the names used when you
+		 * configured your robot and created the configuration file.
+		 */
+
+		/*
+		 * For the demo Tetrix K9 bot we assume the following,
+		 *   There are two motors "motor_1" and "motor_2"
+		 *   "motor_1" is on the right side of the bot.
+		 *   "motor_2" is on the left side of the bot and reversed.
+		 *
+		 * We also assume that there are two servos "servo_1" and "servo_6"
+		 *    "servo_1" controls the arm joint of the manipulator.
+		 *    "servo_6" controls the claw joint of the manipulator.
+		 */
+        motorRightA = hardwareMap.dcMotor.get("motor_fr");
+        motorRightB = hardwareMap.dcMotor.get("motor_br");
+        motorLeftA = hardwareMap.dcMotor.get("motor_fl");
+        motorLeftB = hardwareMap.dcMotor.get("motor_bl");
+        tapeExt = hardwareMap.dcMotor.get("motor_ext");
+        motorLeftA.setDirection(DcMotor.Direction.REVERSE);
+        motorLeftB.setDirection(DcMotor.Direction.REVERSE);
+
+        clawBody = hardwareMap.servo.get("servo_1");
+        trigger = hardwareMap.servo.get("servo_2");
+        tapeAngle = hardwareMap.servo.get("servo_3");
+        plow = hardwareMap.servo.get("servo_4");
+
+
+        trigger.setPosition(.5);
+        clawBody.setPosition(1);
+
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+     */
+    @Override
+    public void loop() {
+
+
+
+		/*
+		 * Gamepad 1
+		 *
+		 * Gamepad 1 controls the motors via the left stick, and it controls the
+		 * wrist/claw via the a,b, x, y buttons
+		 */
+
+        // throttle: left_stick_y ranges from -1 to 1, where -1 is full up, and
+        // 1 is full down
+        // direction: left_stick_x ranges from -1 to 1, where -1 is full left
+        // and 1 is full right
+        float throttle = gamepad1.left_stick_y;
+        float direction = gamepad1.left_stick_x;
+        float right = throttle - direction;
+        float left = throttle + direction;
+
+
+        // clip the right/left values so that the values never exceed +/- 1
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
+
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+        right = (float)scaleInput(right);
+        left =  (float)scaleInput(left);
+
+        // changes direction of motors
+        if(gamepad1.a) {
+            motorRightA.setPower(left);
+            motorRightB.setPower(left);
+            motorLeftA.setPower(right);
+            motorLeftB.setPower(right);
+        }
+        else {
+            motorRightA.setPower(right);
+            motorRightB.setPower(right);
+            motorLeftA.setPower(left);
+            motorLeftB.setPower(left);
+        }
+
+        if(gamepad2.dpad_up){
+            tapePosition += tapeDelta;
+        }
+        if(gamepad2.dpad_down){
+            tapePosition -= tapeDelta;
+        }
+
+        tapePosition = Range.clip(tapePosition, 0, 1);
+
+        tapeAngle.setPosition(tapePosition);
+
+        tapeExt.setPower(gamepad2.right_stick_y*.5);
+
+        if(gamepad1.x){
+            trigger.setPosition(1);
+        }
+
+        if(gamepad1.y){
+            trigger.setPosition(.5);
+        }
+
+        if(gamepad1.b){
+            trigger.setPosition(0);
+        }
+
+        if(gamepad2.right_bumper){
+            clawBody.setPosition(0);
+        }
+
+        if(gamepad2.left_bumper){
+            clawBody.setPosition(1);
+        }
+
+        if(gamepad1.right_trigger>0){
+            plow.setPosition(1);
+        }
+
+        if(gamepad1.left_trigger>0){
+            plow.setPosition(0);
+        }
+
+		/*c
+		 * Send telemetry data back to driver station. Note that if we are using
+		 * a legacy NXT-compatible motor controller, then the getPower() method
+		 * will return a null value. The legacy NXT-compatible motor controllers
+		 * are currently write only.
+		 */
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
+        telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
+        telemetry.addData("Voltage: ", + this.hardwareMap.voltageSensor.iterator().next().getVoltage());
+
+    }
+
+    /*
+     * Code to run when the op mode is first disabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+     */
+    @Override
+    public void stop() {
+
+    }
+
+    /*
+     * Scales the right and left values utilizing the Howerton-Chance Algorithm
+     */
+
+    double scaleInput(double dVal)  {
+        if(dVal>0) {
+            dVal = .4 * (Math.pow(2.71828, (dVal * .4)) - 1);
+        }
+        if(dVal<0) {
+            dVal = -.4 * (Math.pow(2.71828, (dVal * -.4)) - 1);
+        }
+
+        return dVal;
+    }
+
+}
