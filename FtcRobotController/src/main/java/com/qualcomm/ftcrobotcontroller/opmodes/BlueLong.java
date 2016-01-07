000d0f7fa305a80fbc45aa162091a9c8b4773f25
evanhowerton <evanhowerton@gmail.com>
2016-01-07 11:46:04 -0500

    New commit

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BlueLong.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BlueLong.java
@@ -17,7 +17,7 @@ public class BlueLong extends LinearOpMode {
     Servo trigger;
     Servo tapeAngle;
 
-    //Hi
+    //Hello
 
     @Override
     public void runOpMode() throws InterruptedException {
