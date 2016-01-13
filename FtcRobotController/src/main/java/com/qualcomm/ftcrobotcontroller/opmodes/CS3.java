280e980b9cd382ff0dbf36dffe2ebbe5d2b3f9fa
Colby <duncanchance11@gmail.com>
2016-01-13 12:55:34 -0500

    New Test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CS3.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CS3.java
@@ -12,13 +12,23 @@ import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
 
 public class CS3  extends LinearOpMode{
+
     ColorSensor colorSensor;
+    OpticalDistanceSensor ODS;
 
     @Override
     public void runOpMode() throws InterruptedException {
         colorSensor = hardwareMap.colorSensor.get("cs");
+        ODS = hardwareMap.opticalDistanceSensor.get("ods");
         colorSensor.enableLed(true);
         waitOneFullHardwareCycle();
         waitForStart();
@@ -30,10 +40,10 @@ public class CS3  extends LinearOpMode{
         final float values[] = hsvValues;
 
         while(opModeIsActive()) {
+            colorSensor.enableLed(true);
             telemetry.addData("Text", "*** Robot Data***");
             Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
-            //telemetry.addData("Reflectance", "Reflectance Value:  " + reflectance);
-            telemetry.addData("Clear", "Clear Val: " + colorSensor.alpha());
+            telemetry.addData("Refl", "Reflectance Value:  " + ODS.getLightDetected());
             telemetry.addData("Red", "Red Val: " + colorSensor.red());
             telemetry.addData("Blue", "Blue Val: " + colorSensor.blue());
             telemetry.addData("Green", "Green Val: " + colorSensor.green());
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -60,7 +60,6 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register("CS3", CS3.class);
-    manager.register("ColbyTest", COLBYTEST.class);
 
 
 
